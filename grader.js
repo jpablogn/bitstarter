bbcf72e8d3092923acd4bc071f594d005e4d5cd7
jpablogn <mmsjpg@gmail.com>
2013-07-11 23:08:36 +0000

    grader

--- grader.js
+++ grader.js
@@ -24,8 +24,12 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util');
+var rest = require('./restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var HTTPFILE_DEFAULT = "http://powerful-fjord-6608.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -44,8 +48,27 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+// mia
+var loadUrl = function(urlfile){
+	rest.get(urlfile).on('complete', function(result) {
+	  if (result instanceof Error) {
+		sys.puts('Error: ' + result.message);
+		this.retry(5000); // try again after 5 sec
+	  } else {
+		//sys.puts(result);
+		return result;
+	  }
+	});	
+};
+
+var checkHtmlFile = function(htmlfile, checksfile, urlfile) {
+    if(htmlfile){
+		$ = cheerioHtmlFile(htmlfile);
+	}else if(urlfile){
+		var fichero = loadUrl(urlfile); 
+		$ = cheerioHtmlFile(fichero);
+	}
+	
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,8 +88,9 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <http_file>', 'URL path', clone(assertFileExists), HTTPFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson = checkHtmlFile(program.file, program.checks, program.url);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
